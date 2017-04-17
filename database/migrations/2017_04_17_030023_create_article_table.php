47f287f2354fa71f3511e172f0ead89419d20419
xzjs <2310976219@qq.com>
2017-04-17 11:15:11 +0800

    Initial commit

---
+++ database/migrations/2017_04_17_030023_create_article_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('href');
+            $table->text('title');
+            $table->text('tags')->nullable();
+            $table->text('source');
+            $table->string('category');
+            $table->dateTime('releasedate')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
