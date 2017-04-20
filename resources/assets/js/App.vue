77c987084c2f0216550352b78dd24f297fb55699
xzjs <2310976219@qq.com>
2017-04-20 14:19:24 +0800

    在main页添加词组显示入口

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
+
     <router-view></router-view>
-  </div>
+
 </template>
 <script>
-  import Example from './components/Example.vue';
+    import Example from './components/Example.vue';
 
-  export default {
-    name: 'app',
-    components: {
-      Example
-    }
-  };
+    export default {
+        name: 'app',
+        components: {
+            Example
+        }
+    };
 </script>
